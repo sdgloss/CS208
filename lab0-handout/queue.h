d724c78e55d45d2d75542c3a85fbf61b709a167a
sdgloss <63432385+sdgloss@users.noreply.github.com>
2021-12-01 11:54:13 -0600

    Update queue.h

--- lab0-handout/queue.h
+++ lab0-handout/queue.h
@@ -24,7 +24,10 @@ typedef struct list_ele {
 
 /* Queue structure */
 typedef struct {
-    list_ele_t *head;  /* Linked list of elements */
+    list_ele_t *head;
+    list_ele_t *tail;
+    int size;
+      /* Linked list of elements */
     /*
       You will need to add more fields to this structure
       to efficiently implement q_size and q_insert_tail
